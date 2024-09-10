7a4002a245922b52f75bc89be07b4e3f9c6dad3c
Luis Alho <lmalho@gmail.com>
2024-09-10 10:26:40 +0100

    fix database client

--- src/config/config.ts
+++ src/config/config.ts
@@ -20,7 +20,7 @@ export const config = () => {
     db: {
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
+      username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       name: process.env.DB_NAME,
     },
--- src/database/postgres.client.ts
+++ src/database/postgres.client.ts
@@ -1,12 +1,28 @@
-import pgPromise from "pg-promise";
-import { CliConfig } from "src/config/config";
+import * as pgPromise from "pg-promise";
+import { CliConfig } from "../config/config";
+import { IDatabase } from "pg-promise";
+import { IClient } from "pg-promise/typescript/pg-subset";
 
-export const postgresClient = (config: CliConfig) => {
-  return pgPromise()({
+export const postgresClient = (
+  config: CliConfig,
+): IDatabase<any, IClient> | Error => {
+  const pgp = pgPromise({});
+  const db = pgp({
     host: config.db.host,
     port: config.db.port,
     password: config.db.password,
     user: config.db.username,
-    database: "postgres",
+    database: config.db.name,
   });
+
+  db.connect()
+    .then((obj) => {
+      obj.done(); // success, release the connection
+    })
+    .catch((error) => {
+      return Error(
+        `ERROR connecting to the database:${error.message || error}`,
+      );
+    });
+  return db;
 };
