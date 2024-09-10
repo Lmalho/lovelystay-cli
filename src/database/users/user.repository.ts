6a8c9e78c9850388ceab94f214baaea9766028b6
Luis Alho <lmalho@gmail.com>
2024-09-10 22:23:42 +0100

    add user repository

--- src/database/users/user.repository.ts
+++ src/database/users/user.repository.ts
@@ -2,41 +2,99 @@ import { IDatabase } from "pg-promise";
 import { IClient } from "pg-promise/typescript/pg-subset";
 import { GithubUserInfo } from "../../services/github.service";
 import { userLanguagesSql, usersSql } from "./user.sql";
-import { getLanguages } from "../languages/language.repository";
+import { findLanguages } from "../languages/language.repository";
 
-export type User = {
+export type DbUser = {
   id: number;
-  user_name: string;
+  login: string;
   name: string;
   location: string;
   html_url: string;
   repos_url: string;
 };
 
-export const saveUserWithLanguages = async (
+export type User = {
+  id: number;
+  login: string;
+  name: string;
+  location: string;
+  html_url: string;
+  repos_url: string;
+  languages: string[];
+};
+
+export type Filter = {
+  location?: string;
+  language?: string;
+};
+
+export const saveUser = async (
   db: IDatabase<any, IClient>,
   userInfo: GithubUserInfo,
   userLanguages: string[],
 ) => {
-  const languagesIds = await getLanguages(db, userLanguages);
+  const languages = await findLanguages(db, userLanguages);
 
   const newUser = await insertUser(db, userInfo);
 
-  if (languagesIds.length > 0) {
+  if (languages.length > 0) {
     await addUserLanguages(
       db,
       newUser.id,
-      languagesIds.map((l) => l.id),
+      languages.map((l) => l.id),
     );
   }
 
-  return [newUser, languagesIds];
+  return { ...newUser, languages: languages.map((l) => l.name) } as User;
 };
 
+export const findOneUser = async (
+  db: IDatabase<any, IClient>,
+  login: string,
+) => {
+  const result = await db.manyOrNone(usersSql.findOne, login);
+
+  if (!result || result.length === 0) {
+    return null;
+  }
+  const user: User = result.reduce((acc: User, curr) => {
+    return reduceUser(acc, curr);
+  }, null);
+  return user;
+};
+
+export const findUsers = async (
+  db: IDatabase<any, IClient>,
+  filter?: Filter,
+) => {
+  const result = await db.manyOrNone(
+    usersSql.find(filter?.language, filter?.location),
+    [filter?.language || null, filter?.location || null],
+  );
+
+  if (!result || result.length === 0) {
+    return [];
+  }
+
+  const map = new Map<number, User>();
+
+  result.forEach((curr) => {
+    const user = map.get(curr.id);
+    if (user) {
+      reduceUser(user, curr);
+    } else {
+      map.set(curr.id, reduceUser(null, curr));
+    }
+  });
+
+  const users: User[] = Array.from(map.values());
+
+  return users;
+};
 const insertUser = async (
   db: IDatabase<any, IClient>,
   userInfo: GithubUserInfo,
-): Promise<User> => {
+): Promise<DbUser> => {
   return db.one(usersSql.add, [
     userInfo.login,
     userInfo.name,
@@ -56,4 +114,17 @@ const addUserLanguages = async (
       return db.none(userLanguagesSql.add, [userId, l]);
     }),
   );
+};
+
+const reduceUser = (acc: User, curr: any) => {
+  if (!acc) {
+    return {
+      ...curr,
+      languages: curr.languages ? [curr.languages] : [],
+    };
+  }
+  if (!acc.languages.includes(curr.languages)) {
+    acc.languages.push(curr.languages);
+  }
+  return acc;
 };
--- src/database/users/user.sql.ts
+++ src/database/users/user.sql.ts
@@ -1,11 +1,37 @@
 export const usersSql = {
-  add: "INSERT INTO users(user_name, name, location, html_url, repos_url)\
+  add: "INSERT INTO users(login, name, location, html_url, repos_url)\
    VALUES($1, $2, $3, $4, $5)\
+   ON CONFLICT (login) DO UPDATE SET name = $2,\
+   location = $3, html_url = $4, repos_url = $5\
    RETURNING *",
-  find: "",
+  findOne:
+    "SELECT u.login, u.name, u.location, u.html_url, u.repos_url, l.name as languages \
+   FROM users u\
+   LEFT JOIN user_languages ul ON u.id = ul.user_id\
+   LEFT JOIN languages l ON ul.language_id = l.id\
+   WHERE u.login = $1",
+  find: (language?: string, location?: string) => {
+    let query =
+      "SELECT u.id, u.login, u.name, u.location, u.html_url, u.repos_url, l.name as languages \
+    FROM users u\
+    LEFT JOIN user_languages ul ON u.id = ul.user_id\
+    LEFT JOIN languages l ON ul.language_id = l.id\
+    WHERE TRUE";
+
+    if (language) {
+      query += " AND l.name = $1";
+    }
+
+    if (location) {
+      query += " AND u.location = $2";
+    }
+
+    return query;
+  },
 };
 
 export const userLanguagesSql = {
   add: "INSERT INTO user_languages(user_id, language_id)\
-     VALUES($1, $2)",
+     VALUES($1, $2)\
+     ON CONFLICT (user_id, language_id) DO NOTHING",
 };
